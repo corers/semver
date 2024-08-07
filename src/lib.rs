f1fbaf80ccace60cb7762135db531a79deb60b70
liting <luz.liting@gmail.com>
2024-08-07 21:41:58 +0800

    feat(bump_pre): support custion preid

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ pub trait Bump {
     fn bump_patch(&mut self) -> String;
 
     /// bump pre and return version string
-    fn bump_pre(&mut self) -> String;
+    fn bump_pre(&mut self, preid: Option<&str>) -> String;
 }
 impl Bump for Version {
     /// ```
@@ -62,25 +62,80 @@ impl Bump for Version {
     /// ```
     /// use crate::core_semver::{parse,Bump};
     ///
-    /// let mut version = parse("1.2.3-beta.4");
-    /// assert_eq!(version.bump_pre(), "1.2.3-beta.5");
+    /// // default preid: beta
+    /// version = parse("1.2.3");
+    /// assert_eq!(version.bump_pre(None), "1.2.3-beta.1");
     ///
+    /// // default pre value: 1
     /// version = parse("1.2.3-rc");
-    /// assert_eq!(version.bump_pre(), "1.2.3-rc.1");
+    /// assert_eq!(version.bump_pre(None), "1.2.3-rc.1");
+    ///
+    /// let mut version = parse("1.2.3-beta.4");
+    /// assert_eq!(version.bump_pre(None), "1.2.3-beta.5");
+    /// assert_eq!(version.bump_pre(Some("rc")), "1.2.3-rc.1");
     /// ```
-    fn bump_pre(&mut self) -> String {
+    fn bump_pre(&mut self, preid: Option<&str>) -> String {
+        let mut has_preid = false;
+        let current_preid;
+        match preid {
+            Some(value) => {
+                if value.trim().len() == 0 {
+                    current_preid = "beta";
+                } else {
+                    current_preid = value.trim();
+                    has_preid = true;
+                }
+            }
+            None => {
+                current_preid = "beta";
+            }
+        }
+
+        if self.pre.is_empty() {
+            self.pre = Prerelease::new(format!("{}.{}", current_preid, "1").as_str()).unwrap();
+            return self.to_string();
+        }
+
         let mut pre_arr = self.pre.as_str().split(".").collect::<Vec<&str>>();
-        let pre_value = pre_arr.last().unwrap();
+        if pre_arr.len() == 0 {
+            pre_arr.push(&current_preid);
+            pre_arr.push("1");
+            self.pre = Prerelease::new(pre_arr.join(".").as_str()).unwrap();
+            return self.to_string();
+        }
+
+        let pre_value = pre_arr.pop().unwrap();
         #[allow(unused_assignments)]
         let mut next_value = pre_value.to_string();
 
         match pre_value.parse::<u32>() {
             Ok(value) => {
-                pre_arr.pop();
-                next_value = (value + 1).to_string();
-                pre_arr.push(next_value.as_str());
+                if has_preid {
+                    if current_preid == pre_arr.join(".") {
+                        next_value = (value + 1).to_string();
+                        pre_arr.push(next_value.as_str());
+                    } else {
+                        pre_arr.clear();
+                        pre_arr.push(&current_preid);
+                        pre_arr.push("1");
+                    }
+                } else {
+                    if pre_arr.len() == 0 {
+                        pre_arr.push(&current_preid);
+                    }
+
+                    next_value = (value + 1).to_string();
+                    pre_arr.push(next_value.as_str());
+                }
             }
             Err(_) => {
+                if has_preid {
+                    pre_arr.clear();
+                    pre_arr.push(&current_preid);
+                } else {
+                    pre_arr.push(pre_value);
+                }
+
                 pre_arr.push("1");
             }
         }
